69141611ebd83795513bf1b0c4fa6b6f0b150594
thang1405 <43245805+thang1405@users.noreply.github.com>
2018-09-28 12:51:41 +0700

    complete homeworks

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -4,12 +4,13 @@ public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
     Student[] students = new Student[100];
+
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
         if ( s1.getGroup()== s2.getGroup()) return true;
         return false; // xóa dòng này sau khi cài đặt
     }
-    int number(){ // đếm số phần tử của mảng
+    int num(){ // đếm số phần tử của mảng
         int i=0;
         while(students[i]!=null) i++;
         return i;
@@ -18,8 +19,8 @@ public class StudentManagement {
         //TODO:
     Student a = new Student();
     String cla = students[0].getGroup(); //khởi tạo biến class để nhận tên lớp
-        for(int i =0;i<number();i++){
-            for(int j =i+1;j<number();j++){
+        for(int i =0;i<num();i++){
+            for(int j =i+1;j<num();j++){
                 if(students[i].getGroup().equals(students[j].getGroup())) {
                     a=students[i];
                     students[i]=students[j]; // sắp xếp lại mảng
@@ -28,32 +29,31 @@ public class StudentManagement {
         }
     }
 		System.out.println(cla); // in tên các class
-		 for(int i=0;i<number();i++){
+		 for(int i=0;i<num();i++){
         if(students[i].getGroup().equals(cla)) students[i].getInfo(); // in thông tin
         else {
             cla = students[i].getGroup(); // đặt lại biến class để in lớp khác
             System.out.println(cla);     // lớp tiếp theo
             students[i].getInfo();
-            //i--;
+
         }
     }
 }
 
     void removeStudent(String id) {
         //TODO:
-        for(int i=0; i<number(); i++)
+        for(int i=0; i<num(); i++)// chạy mảng
         {
-
-            if(id.equals(students[i].getId()))
+            if(id.equals(students[i].getId()))// so sánh với id trong các mảng
             {
-                for(int j=i; j<number(); j++)
+                for(int j=i; j<num(); j++)
                 {
-                    students[j] = students[j + 1];
+                    students[j] = students[j + 1];// xóa khỏi ds
                 }
             }
         }
         System.out.println("Danh sach lop sau khi xoa: ");
-        for(int i=0;i<number();i++) students[i].getInfo();
+        for(int i=0;i<num();i++) students[i].getInfo();// in ra ds sau khi xóa
     }
 
     public static void main(String[] args) {
@@ -75,13 +75,14 @@ public class StudentManagement {
         Student st3 = new Student(stu);
         st3.getInfo();
 
-        Student s1= new Student("tran manh thang 1","17021034","pohajtak@gmail.com");
-        Student s2= new Student("tran manh thang 2","17021034","pohajtak@gmail.com");
+        Student s1= new Student("tran manh thang 1","17021035","pohajtak@gmail.com");
+        Student s2= new Student("tran manh thang 2","17021036","pohajtak@gmail.com");
         Student s3 = new Student();
         s3.setName("tran manh thang 3");
-        s3.setId("000");
+        s3.setId("17021037");
         s3.setGroup("INT22042");
         s3.setEmail("pohajtak@gmail.com");
+
 
         StudentManagement a= new StudentManagement();
             if(a.sameGroup(s1,s2)== true )
@@ -98,7 +99,7 @@ public class StudentManagement {
 
             a.studentsByGroup();
             System.out.println("\n");
-            a.removeStudent("000");
+            a.removeStudent("17021034");
 
 
     }
