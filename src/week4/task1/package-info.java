b1ebc2b517220a98bd96b6c2108e89ac314232f3
thang1405 <43245805+thang1405@users.noreply.github.com>
2018-10-12 11:56:52 +0700

    update hw

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -2,8 +2,64 @@
  * Package bài làm cho Câu 1
  * TODO: Khai báo các lớp (class) theo Câu 1 yêu cầu. 1 file chỉ khai báo 1 class
  * TODO: Mô tả các quan hệ giữa các class và thêm thuộc tính, phương thức cho chúng (mỗi class ít nhất 2 phương thức, 2 thuộc tính)
- * TODO: Viết comment đầy đủ cho class, phương thức. Xem hướng dẫn tại đây (http://dongdiemthuy.github.io/java/javadoc/2014/09/30/su-dung-javadoc-va-maven-de-tao-tai-lieu-du-an.html)
+ * TODO: Viết comment đầy đủ cho class, phương thức. Xem hướng dẫn tại đây 
+ * (http://dongdiemthuy.github.io/java/javadoc/2014/09/30/su-dung-javadoc-va-maven-de-tao-tai-lieu-du-an.html)
  * @author cuong
  * @version 0.1
  */
 package week4.task1;
+
+//public class HoaQua{
+//    private
+//    int giaban;
+//    String Xuatxu;
+//    int soluong_kg;
+//
+//    public int getGiaban() {
+//        return giaban; }
+//    public int getSoluong_kg() {
+//        return soluong_kg; }
+//    public String getXuatxu() {
+//        return Xuatxu; }
+//    public void setGiaban(int giaban) {
+//        this.giaban = giaban; }
+//    public void setXuatxu(String xuatxu) {
+//        Xuatxu = xuatxu; }
+//    public void setSoluong_kg(int soluong_kg) {
+//        this.soluong_kg = soluong_kg; }
+//}
+
+//public class Tao extends HoaQua{
+//    private
+//    String loaiTao;
+//    public void setLoaiTao(String loaiTao) {
+//        this.loaiTao = loaiTao; }
+//    public String getLoaiTao() {
+//        return loaiTao; }
+//}
+//public class Cam extends HoaQua{
+//    private
+//    String Vocam;
+//    public void setVocam(String vocam) {
+//        Vocam = vocam; }
+//    public String getVocam() {
+//        return Vocam; }
+//}
+//public class CamThanhPhong extends Cam{
+//
+//
+//
+//}
+//public class CamSanh extends Cam{
+//
+//
+//}
+
+//public static void main(String[] args) {
+//    Tao tao= new Tao();
+//    tao.getXuatxu("tau khua");
+//    tao.setGiaban(50000);
+//    tao.setSoluong_kg(5);
+//    tao.setLoaiTao("Tao Tau");
+//    System.out.println(tao.getLoaiTao());
+//}
